ee907c4a6ca19620e73081fec0dccf0bd2904259
azzam-2021 <azzam1212@student.abudzar.sch.id>
2025-02-21 08:32:35 +0700

    first commit

---
+++ biodata.js
@@ -0,0 +1,7 @@
+var nama="Azzam ";
+var umur="14 ";
+var alamat="Tangerang selatan ";
+var hobi="gatau ";
+var fusion=nama + umur + alamat + hobi;
+console.log(fusion);
+document.writeln(nama+umur+alamat+hobi)
---
+++ script.js
@@ -0,0 +1,12 @@
+var nama="budi"
+var umur=14
+var kota="jakarta"
+var gabung=nama + kota + umur
+console.log(gabung)
+
+document.writeln("hello world")
+document.writeln(nama+umur+kota)
+document.writeln(1000*99999)
+// tipe data number
+console.log(typeof(umur))
+console.log(typeof(kota))
